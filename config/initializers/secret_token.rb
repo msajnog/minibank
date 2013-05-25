61399b22f871e9823309e1e5b2c6a7d0d74ff8bb
msajnog <msajnog@ubuntu.(none)>
2013-05-25 18:56:36 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Minibank::Application.config.secret_key_base = 'd87ea49a5af56dcefa5677a3c9864e88024a20a764640dbf719e04def16b4e844327446ceb871c6124acd9b76ee76b776f42418c1efec1ed827c461b207be91b'
